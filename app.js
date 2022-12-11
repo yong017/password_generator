fc4c0641cbe8873470a8062adc51743d128aad97
yong017 <zero0lemon@gmail.com>
2022-12-11 13:53:55 +0800

    password-generator

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+const app = express()
+const port = 3000
+
+// setting template engine
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+// setting body-parser
+app.use(express.urlencoded({ extended: true }))
+
+// setting routes
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+app.post('/', (req, res) => {
+  console.log('req.body', req.body)
+  res.render('index')
+})
+
+// starts the express server and listening for connections.
+app.listen(port, () => {
+  console.log(`listening on port ${port}`)
+})
